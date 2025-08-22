7300e757311ec22ee758c82feb9814f21db33722
Pinsoe Gustave <ntwalimudasubirag@gmail.com>
2025-08-22 13:23:15 -0400

    Created navbar and added links

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,9 +1,11 @@
+import Navbar from '@/components/Navbar'
 import React, { ReactNode } from 'react'
 
 function layout({ children }: { children: ReactNode}) {
   return (
     <div className='relative flex h-screen w-full flex-col '>
-        <div className='w-full'>
+        <Navbar />
+        <div className='w-full'>            
             { children }
         </div>
     </div>
