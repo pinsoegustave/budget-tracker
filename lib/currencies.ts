0ac0e60f5be762a5c2d0549a8bc406641fbaa102
Pinsoe Gustave <ntwalimudasubirag@gmail.com>
2025-08-29 19:34:48 -0400

    Created the currencies object

---
+++ lib/currencies.ts
@@ -0,0 +1,26 @@
+export const Currencies = [
+    {
+        value: "USD",
+        label: "$ Dollar",
+        locale: "en-US"
+    },
+    {
+        value: "RWF",
+        label: "RWF",
+        locale: "en-US"
+    },
+
+    {
+        value: "EUR",
+        label: " Euro",
+        locale: "en-US"
+    },
+
+    {
+        value: "GBP",
+        label: " Pound",
+        locale: "en-GB"
+    },
+]
+
+export type Currency = (typeof Currencies)[0];
