86643489d72ed252d72428f632d33480e926d73a
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-11-17 23:38:33 -0500

    Set Currency, Spinner, toast messages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+// import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import RootProviders from "@/components/providers/RootProviders";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,7 @@ export default function RootLayout({
     >   
       <body >
         <RootProviders>
+          <Toaster richColors position="bottom-right" />
           {children}
         </RootProviders>
       </body>
